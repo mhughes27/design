2f2eb3b6e1a487f9091b8e53b198d65ae33e6ebf
Matt Hughes <mhughes27@gmail.com>
2016-07-05 09:47:45 -0500

    starting design

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Design Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+
+<body class="supercontainer">
+	<div class="container">
+		<div class="leftcloumn">
+
+		</div>
+		<div class="maincontent">
+		</div>
+
+		<div class="rightcolumn">
+		</div>
+	</div>
+
+
+
+</body>
+</html>
